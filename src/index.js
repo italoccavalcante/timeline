c81da645eabd19c3fc50ea400a13f33efe8b7a15
italoccavalcante <italoccavalcante@hotmail.com>
2025-04-12 12:35:19 -0300

    feat: changing main page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import timelineItems from "./data/timelineItems.js";
+import {Timeline} from './components/timeline'
 import {assignLanes} from './utils/assignLanes.js'
 function App() {
+  const timeLineOrdered = assignLanes(timelineItems)
   return (
-    <div>
-      <h2>Good luck with your assignment! {"\u2728"}</h2>
-      {console.log(assignLanes(timelineItems))}
+    <div style={{textAlign:'center'}}>
+      <h2 >TIMELINE {"\u2728"}</h2>
+      {timeLineOrdered.map((timeLine, index) => {
+        return (
+          <Timeline key={index} items={timeLine}/>
+        )
+      })}
       <h3>{timelineItems.length} timeline items to render</h3>
     </div>
   );
