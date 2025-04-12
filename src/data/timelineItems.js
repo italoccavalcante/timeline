0747db4a32b75ebe28b77dea24823d39e58b9238
italoccavalcante <italoccavalcante@hotmail.com>
2025-04-12 12:34:45 -0300

    feat: adding color prop

--- src/data/timelineItems.js
+++ src/data/timelineItems.js
@@ -4,96 +4,113 @@ const timelineItems = [
     start: "2021-01-14",
     end: "2021-01-22",
     name: "Recruit translators",
+    color:'orange'
   },
   {
     id: 2,
     start: "2021-01-17",
     end: "2021-01-31",
     name: "Create lesson plan 1",
+    color:'green'
   },
   {
     id: 3,
     start: "2021-02-05",
     end: "2021-02-13",
     name: "Translate phrases for lesson 1",
+    color:'purple'
   },
   {
     id: 4,
     start: "2021-02-07",
     end: "2021-03-08",
     name: "Create dark mode design",
+    color:'blue'
+    
   },
   {
     id: 5,
     start: "2021-02-14",
     end: "2021-02-22",
     name: "Recruit copyeditors",
+    color:'orange'
   },
   {
     id: 6,
     start: "2021-02-18",
     end: "2021-02-24",
     name: "Proofread translations for lesson 1",
+    color:'purple'
   },
   {
     id: 7,
     start: "2021-02-20",
     end: "2021-02-22",
     name: "Finalize logo",
+    color:'green'
   },
   {
     id: 8,
     start: "2021-02-21",
     end: "2021-03-22",
     name: "Implement dark mode",
+    color:'blue'
   },
   {
     id: 9,
     start: "2021-02-21",
     end: "2021-02-28",
     name: "Finalize lesson plan 1",
+    color:'orange'
   },
   {
     id: 10,
     start: "2021-02-23",
     end: "2021-02-23",
     name: "Approve logo",
+    color:'purple'
   },
   {
     id: 11,
     start: "2021-03-03",
     end: "2021-03-29",
     name: "Create lesson plan 2",
+    color:'orange'
   },
   {
     id: 12,
     start: "2021-03-30",
     end: "2021-04-08",
     name: "Translate phrases for lesson 2",
+    color:'green'
   },
   {
     id: 13,
     start: "2021-04-01",
     end: "2021-04-04",
     name: "Debug mobile notification error",
+    color:'purple'
   },
   {
     id: 14,
     start: "2021-04-05",
     end: "2021-04-06",
     name: "Test debugged mobile notifications",
+    color:'orange'
   },
   {
     id: 15,
     start: "2021-04-16",
     end: "2021-04-30",
     name: "Beta test",
+    color:'purple'
   },
   {
     id: 16,
     start: "2021-05-01",
     end: "2021-05-01",
     name: "Launch day",
+    color:'green'
   },
 ];
 
