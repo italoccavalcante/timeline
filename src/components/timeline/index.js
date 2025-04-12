d076c9302f0a564ed5076ecac4efc24a4e95449a
italoccavalcante <italoccavalcante@hotmail.com>
2025-04-12 12:35:04 -0300

    feat: adding timesheet component

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -1,8 +1,31 @@
-import './styles.css'
-export const Timeline = () => {
-    return(
-        <div>
+import './style.css'
+import moment from 'moment'
+export const Timeline = (props) => {
+    const {items} = props
+
+    if(items.length === 0) {
+        return (
+            <h1>No data Founded</h1>
+        )
+    }
+
+
+    return (
+        <div  className='container'> 
+            {items.map((items) => {
+                const startDate = moment(items.start).format("MMM YYYY")
+                const cardColor = items.color ? items.color : 'default'
+                return (
+                    <div key={items.id} className={`itemCard ${cardColor}`}>
+                        <h1>{startDate}</h1>
+                        <span>{items.name}</span>
+                    </div>
+    
             
+            
+            )
+            })}
         </div>
     )
+    
 }
